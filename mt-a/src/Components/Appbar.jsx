f056a6142da8b636de99294be9ebd46a398f13d4
SebastianLennstrom <117740646+SebastianLennstrom@users.noreply.github.com>
2024-12-15 22:43:08 +0100

    fixat animation. laggt till bilder och navigations alternativ

--- mt-a/src/Components/Appbar.jsx
+++ mt-a/src/Components/Appbar.jsx
@@ -8,13 +8,10 @@ function Appbar() {
         <div className="header">
             <nav className="header_nav">
                 <ul className="list">
-                    <li>
-                        <Link to="/MT-A/">Home</Link>
-                    </li>
-                    {/* Uncomment and add routes when needed */}
-                    {/* <li><Link to="/about">About</Link></li> */}
-                    {/* <li><Link to="/contact">Contact</Link></li> */}
-                    {/* <li><Link to="/schema">Schema</Link></li> */}
+                    <li><Link to="/MT-A/">Home</Link></li>
+                    <li><Link to="/MT-A/">About</Link></li> 
+                    <li><Link to="/MT-A/">Contact</Link></li> 
+                    <li><Link to="/MT-A/">Schema</Link></li> 
                 </ul>
             </nav>
         </div>
